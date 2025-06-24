b241888d700e383d18e3de289da020700fa24977
Preetish Bakliwal <pbakliwal@adobe.com>
2025-06-24 17:16:48 +0530

    Update asset-upload.js and copilot-init.js to use new demo system URLs and append IMS parameter to CSS and JS links

--- scripts/asset-upload.js
+++ scripts/asset-upload.js
@@ -68,7 +68,7 @@ const fetchProjectData = async (projectId) => {
             throw new Error('Authentication token not found');
         }
 
-        const response = await fetch(`https://xf6x9c9l4l.execute-api.us-east-1.amazonaws.com/projects/${projectId}`, {
+        const response = await fetch(`https://demo-system-zoltar-demo-pilot-deploy-ethos101-stag-6229b6.stage.cloud.adobe.io/projects/${projectId}`, {
             headers: {
                 'Authorization': `Bearer ${token}`,
                 'Accept': 'application/json'
@@ -315,7 +315,7 @@ const updateTargetUrl = async (projectId, demoId,targetUrl) => {
         };
 
         // Send PATCH request
-        const patchUrl = `https://xf6x9c9l4l.execute-api.us-east-1.amazonaws.com/projects/${projectId}/demos/${demoId}`;
+        const patchUrl = `https://demo-system-zoltar-demo-pilot-deploy-ethos101-stag-6229b6.stage.cloud.adobe.io/projects/${projectId}/demos/${demoId}`;
         const response = await fetch(patchUrl, {
             method: 'PATCH',
             headers: {
--- scripts/copilot-init.js
+++ scripts/copilot-init.js
@@ -19,7 +19,7 @@ if (shouldLoadCopilot) {
                 link.id = 'copilot-editor-css';
                 link.rel = 'stylesheet';
                 link.type = 'text/css';
-                link.href = `https://${domain}/editor/editor.css`;
+                link.href = `https://${domain}/editor/editor.css?ims=explicit`;
                 link.media = 'all';
                 
                 // Handle loading errors
@@ -36,7 +36,7 @@ if (shouldLoadCopilot) {
                 const script = document.createElement('script');
                 script.id = 'copilot-editor-script';
                 script.type = 'module';
-                script.src = `https://${domain}/editor/editor.js`;
+                script.src = `https://${domain}/editor/editor.js?ims=explicit`;
                 
                 // Handle loading errors
                 script.onerror = () => console.error('Failed to load copilot script');
